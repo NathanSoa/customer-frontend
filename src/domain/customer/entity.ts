060e3a2c1cb9b458d0be3ac4f59896731774ea9a
Nathan Soares <nathansoares809@gmail.com>
2023-11-03 18:27:09 -0300

    feat: create customer domain interface

---
+++ src/domain/customer/entity.ts
@@ -0,0 +1,40 @@
+import { BaseEntity } from '@/domain/common/base-entity'
+
+export interface Phone {
+  type: string
+  ddd: string
+  number: string
+}
+
+export interface Address {
+  type: string
+  streetType: string
+  street: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  country: string
+  zipCode: string
+  complement?: string
+}
+
+export interface Card {
+  type: string
+  name: string
+  number: string
+  securityCode: string
+  flag: string
+  main: boolean
+}
+
+export interface Customer extends BaseEntity {
+  name: string
+  cpf: string
+  birthdate: Date
+  email: string
+  password: string
+  phone: Phone
+  address: Address[]
+  card: Card[]
+}
