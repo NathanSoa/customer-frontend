1d3dd9ae2e7f5a83d30fe46706f1dbaba88a52d4
Nathan Soares <nathansoares809@gmail.com>
2023-11-21 15:45:25 -0300

    feat: create custom hook for validation

---
+++ src/app/hooks/useValidator.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { z } from 'zod'
+
+export function useValidator(zodValidator: z.ZodObject<any>) {
+  const [errors, setErrors] = useState<any>({})
+  const [validator, setValidator] = useState(zodValidator)
+
+  function validate(data: any) {
+    const result = validator.safeParse(data)
+    if (!result.success) {
+      const error: any = {}
+      result.error.issues.forEach((issue) => {
+        error[issue.path[0] as string] = issue.message
+      })
+      setErrors(error)
+      return false
+    } else {
+      return true
+    }
+  }
+
+  return {
+    errors,
+    validate,
+    setValidator,
+  }
+}
