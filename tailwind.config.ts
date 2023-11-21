52672797d33cb332d2eed2d42205db3101465f7f
Nathan Soares <nathansoares809@gmail.com>
2023-11-21 15:41:20 -0300

    feat: add grid templates config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
       },
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
+        'aligned-itens-2': '1fr 1fr',
+        'aligned-itens-3': '1fr 1fr 1fr',
       },
       padding: {
         tableCell: '0.75rem 1.25rem',
