48f7759a27c69575f8337f77434cbe8b0d8dc83b
Nathan Soares <nathansoares809@gmail.com>
2023-11-22 09:58:38 -0300

    feat: add base setup for rendering errors

--- src/app/customers/components/main-form/index.tsx
+++ src/app/customers/components/main-form/index.tsx
@@ -12,9 +12,17 @@ import Form from '@/app/components/form'
 
 import { useModal } from '@/app/hooks/useModal'
 import { useCustomer } from '@/app/hooks/useCustomer'
+import { useValidator } from '@/app/hooks/useValidator'
+
+import { CustomerZodValidation } from '@/validation/customer-zod-validation'
+
+import { Customer, CustomerCreate } from '@/domain/customer/entity'
+
+import { handleInputData } from '@/utils/handle-input-data'
 
 import { Star, X } from 'phosphor-react'
 import classNames from 'classnames'
+import { useEffect, useState } from 'react'
 
 export default function MainForm() {
   const {
@@ -38,25 +46,79 @@ export default function MainForm() {
     openModal: openCardModal,
   } = useModal()
 
+  const [renderErrors, setRenderErrors] = useState<any>({})
+  const { validate, errors } = useValidator(CustomerZodValidation)
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+    const formLength = e.target.form.length
+
+    const formData: Partial<CustomerCreate> = {}
+
+    for (let i = 0; i < formLength; i++) {
+      handleInputData(formData, e.target.form[i])
+    }
+
+    const data = {
+      ...formData,
+      address: customer?.address,
+      cards: customer?.cards,
+      phone: {
+        type: 'Celular',
+        ddd: formData.ddd,
+        number: formData.phone,
+      },
+      ddd: undefined,
+      'password-confirm': undefined,
+    }
+
+    console.log(data)
+
+    const success = validate(data)
+
+    if (success) {
+      console.log('Sucesso')
+      close()
+    }
+  }
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) {
+      setRenderErrors(errors)
+    }
+  }, [errors])
+
   return (
     <Form>
       <InputContainer>
         <label htmlFor="name">Nome *</label>
+        {renderErrors.name && (
+          <span className="text-sm text-red-500">{renderErrors.name}</span>
+        )}
         <Input type="text" name="name" id="name" />
       </InputContainer>
 
       <AlignedInputs.Two>
         <InputContainer>
           <label htmlFor="cpf">CPF *</label>
+          {renderErrors.cpf && (
+            <span className="text-sm text-red-500">{renderErrors.cpf}</span>
+          )}
           <Input type="cpf" name="cpf" id="cpf" />
         </InputContainer>
         <AlignedInputs.Two>
           <InputContainer>
             <label htmlFor="ddd">DDD *</label>
+            {renderErrors.ddd && (
+              <span className="text-sm text-red-500">{renderErrors.ddd}</span>
+            )}
             <Input type="text" name="ddd" id="ddd" maxLength={2} />
           </InputContainer>
           <InputContainer>
             <label htmlFor="phone">Telefone *</label>
+            {renderErrors.phone && (
+              <span className="text-sm text-red-500">{renderErrors.phone}</span>
+            )}
             <Input type="text" name="phone" id="phone" maxLength={9} />
           </InputContainer>
         </AlignedInputs.Two>
@@ -65,10 +127,18 @@ export default function MainForm() {
       <AlignedInputs.Two>
         <InputContainer>
           <label htmlFor="email">E-mail *</label>
+          {renderErrors.email && (
+            <span className="text-sm text-red-500">{renderErrors.email}</span>
+          )}
           <Input type="email" name="email" id="email" />
         </InputContainer>
         <InputContainer>
           <label htmlFor="birthdate">Data de Nascimento *</label>
+          {renderErrors.birthdate && (
+            <span className="text-sm text-red-500">
+              {renderErrors.birthdate}
+            </span>
+          )}
           <Input type="text" name="birthdate" id="birthdate" />
         </InputContainer>
       </AlignedInputs.Two>
@@ -76,10 +146,20 @@ export default function MainForm() {
       <AlignedInputs.Two>
         <InputContainer>
           <label htmlFor="password">Senha *</label>
+          {renderErrors.password && (
+            <span className="text-sm text-red-500">
+              {renderErrors.password}
+            </span>
+          )}
           <Input type="password" name="password" id="password" />
         </InputContainer>
         <InputContainer>
           <label htmlFor="password-confirm">Confirme a senha *</label>
+          {renderErrors['password-confirm'] && (
+            <span className="text-sm text-red-500">
+              {renderErrors['password-confirm']}
+            </span>
+          )}
           <Input
             type="password"
             name="password-confirm"
@@ -90,6 +170,9 @@ export default function MainForm() {
 
       <div>
         <h2 className="text-lg">Endereço(s) *</h2>
+        {renderErrors.address && (
+          <span className="text-sm text-red-500">{renderErrors.address}</span>
+        )}
         <div className="py-3 font-semibold">
           É necessário ter um endreço de cobrança e de entrega
         </div>
@@ -100,7 +183,7 @@ export default function MainForm() {
                 <div className="flex justify-between">
                   <span>{`${address.street}, nº ${address.number}`}</span>
                   <X
-                    className="h-6 w-6 cursor-pointer hover:text-red-500"
+                    className="h-6 w-6 cursor-pointer text-red-400 hover:text-red-500"
                     onClick={() => removeAddress(address)}
                   />
                 </div>
@@ -117,6 +200,9 @@ export default function MainForm() {
 
       <div>
         <h2 className="text-lg">Cartão(ões) *</h2>
+        {renderErrors.cards && (
+          <span className="text-sm text-red-500">{renderErrors.cards}</span>
+        )}
         <div className="grid grid-cols-aligned-itens-3 gap-1 py-2">
           {customer !== null &&
             customer.cards.map((card, index) => (
@@ -134,7 +220,7 @@ export default function MainForm() {
                     {card.flag}
                   </div>
                   <X
-                    className="h-6 w-6 cursor-pointer hover:text-red-500"
+                    className="h-6 w-6 cursor-pointer text-red-400 hover:text-red-500"
                     onClick={() => removeCard(card)}
                   />
                 </div>
@@ -147,9 +233,7 @@ export default function MainForm() {
       </div>
 
       <div className="mt-4 flex justify-end">
-        <Button.Green onClick={() => console.log('Adicionar Cartão')}>
-          Salvar
-        </Button.Green>
+        <Button.Green onClick={(e) => handleSubmit(e)}>Salvar</Button.Green>
       </div>
 
       {showAddressModal && (
