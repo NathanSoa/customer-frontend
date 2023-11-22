6e326cec899e291676b16dade094d3590c29f634
Nathan Soares <nathansoares809@gmail.com>
2023-11-22 13:30:58 -0300

    feat: add delete modal and old action section component

--- src/app/components/table/table-row/index.tsx
+++ src/app/components/table/table-row/index.tsx
@@ -1,18 +1,57 @@
-import ActionSection from '@/app/components/table/action-section'
+import DeleteModal from '@/app/components/delete-modal'
+
+import { useModal } from '@/app/hooks/useModal'
+
+import { getDataToDisplay } from '@/utils/get-data-to-display'
+
+import { Pencil, Trash } from 'phosphor-react'
 
 interface TableBodyProps {
-  data: any[]
+  data: any
 }
 
 export default function TableBody({ data }: TableBodyProps) {
+  const {
+    showModal: showDeleteModal,
+    closeModal: closeDeleteModal,
+    openModal: openDeletModal,
+  } = useModal()
+
+  const dataToDisplay = getDataToDisplay(data)
+
   return (
     <tr className="bg-white">
-      {data.map((cell, index) => (
+      {dataToDisplay.map((cell, index) => (
         <td className="p-tableCell" key={index}>
           {cell}
         </td>
       ))}
-      <ActionSection />
+      <td className="p-tableCell">
+        <div className="justify-left flex items-center gap-1">
+          <Pencil
+            size={24}
+            alt="Editar"
+            className="cursor-pointer hover:text-green-400"
+          />
+          <Trash
+            size={24}
+            alt="Apagar"
+            className="cursor-pointer hover:text-red-500"
+            onClick={openDeletModal}
+          />
+        </div>
+        {showDeleteModal && (
+          <DeleteModal
+            handleDelete={(id: string) => {
+              console.log(id)
+            }}
+            onHide={closeDeleteModal}
+            title="Apagar Entidade"
+            entityId={data.id}
+            entityName={data.name}
+          />
+        )}
+      </td>
     </tr>
   )
 }
