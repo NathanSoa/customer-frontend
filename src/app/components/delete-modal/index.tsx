21b7d073e5147797e3b6889f3081a62d60d8f177
Nathan Soares <nathansoares809@gmail.com>
2023-11-22 13:30:37 -0300

    feat: create delete modal

---
+++ src/app/components/delete-modal/index.tsx
@@ -0,0 +1,36 @@
+import Modal from '@/app/components/modal'
+import Button from '../button'
+
+interface DeleteModalProps {
+  handleDelete: (id: string) => void
+  onHide: () => void
+  title: string
+  entityName: string
+  entityId: string
+}
+
+export default function DeleteModal({
+  handleDelete,
+  onHide,
+  title,
+  entityName,
+  entityId,
+}: DeleteModalProps) {
+  return (
+    <Modal onHide={onHide} title={title}>
+      <div className="flex h-40 flex-col items-center justify-center gap-6">
+        <p className="text-center">
+          Você tem certeza que deseja apagar <strong>{entityName}</strong>?
+        </p>
+        <Button.Red
+          onClick={() => {
+            handleDelete(entityId)
+            onHide()
+          }}
+        >
+          Apagar
+        </Button.Red>
+      </div>
+    </Modal>
+  )
+}
